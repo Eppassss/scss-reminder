a4d181a888e97f769ce374fca84d9369b3299485
patrickli147 <49908827+patrickli147@users.noreply.github.com>
2021-11-12 16:46:23 +0800

    chore: load more info of variables

--- server/src/loadVariables.ts
+++ server/src/loadVariables.ts
@@ -1,32 +1,51 @@
 import * as fs from 'fs';
 
 import {
+	Position,
 	TextDocument
 } from 'vscode-languageserver-textdocument';
 
+import {scssVariablePattern} from './utils/regExp';
+
 export interface IVariableData {
-	start: number;
-	end: number;
+	/**
+	 * start position
+	 */
+	start: Position;
+	/**
+	 * end position
+	 */
+	end: Position;
+	/**
+	 * variable value
+	 */
 	value: string;
+	/**
+	 * original content of variable definition
+	 */
 	origin: string;
+	/**
+	 * uri of textDocument
+	 */
+	uri: string;
 }
 
 export default (path: string) => {
 	const text = fs.readFileSync(path, 'utf-8');
 	const cssTextDocument = TextDocument.create(path.substr(1), "plaintext", 1, text);
-	const pattern = /(\$[\w-]+:)(\s)*([^;\n$]*)/g;
 	const variables: Map<string, IVariableData> = new Map();
 
 	let matches: RegExpExecArray | null;
 
-	while ((matches = pattern.exec(text)) !== null) {
+	while ((matches = scssVariablePattern.exec(text)) !== null) {
 		const variableName = matches[1];
 		const variableValue = matches[3];
 		const variableData = {
-			start: matches.index,
-			end: matches.index + matches[0].length,
+			start: cssTextDocument.positionAt(matches.index),
+			end: cssTextDocument.positionAt(matches.index + matches[0].length),
 			value: variableValue,
-			origin: matches[0]
+			origin: matches[0],
+			uri: cssTextDocument.uri
 		};
 		variables.set(variableName, variableData);
 	}
